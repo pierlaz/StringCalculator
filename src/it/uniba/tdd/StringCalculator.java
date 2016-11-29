0888325dbd38144c33f9ebba38bcb65844338603
Pierluigi Lazazzera <pierluigi.lazazzera@gmail.com>
2016-11-29 17:00:52 +0100

    all done

--- src/it/uniba/tdd/StringCalculator.java
+++ src/it/uniba/tdd/StringCalculator.java
@@ -1,9 +1,55 @@
 package it.uniba.tdd;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		// not yet implemented
-		return 0;
+
+	public static int add(String numbersStr) throws StringCalculatorException {
+		final String startWithDelimiters = "//";
+		final String newLine = "\\n";
+		String[] delimiter = new String[1];
+
+		String defaultDelimiter = ",|\\n";
+
+		if (numbersStr == "") {
+			return 0;
+		}
+		if (numbersStr.charAt(0) == '/' && numbersStr.charAt(1) == '/') {
+			delimiter = numbersStr.split(startWithDelimiters + "|" + newLine);
+			if (delimiter[1].contains("[") && delimiter[1].contains("]")) {
+				delimiter[1] = delimiter[1].substring(1, delimiter[1].length() - 1);
+				String[] delims = delimiter[1].split("\\]\\[");
+				String s = "";
+				for (int i=0; i<delims.length; i++) {
+					s += delims[i] + '|';
+				}
+				delimiter[1] = s;
+				delimiter[1] = delimiter[1].substring(0, delimiter[1].length()-1);
+			}
+			defaultDelimiter = delimiter[1];
+			if (defaultDelimiter.contains("-")) {
+				char[] temp = numbersStr.toCharArray();
+				for (int i = 0; i < numbersStr.length() - 1; i++) {
+					if (temp[i] == '-' && '-' == temp[i + 1]) {
+						throw new StringCalculatorException();
+					}
+				}
+			}
+			String[] tempo = numbersStr.split(newLine);
+			numbersStr = tempo[1];
+		}
+
+		int sum = 0;
+
+		String[] parts = numbersStr.split(defaultDelimiter);
+
+		for (String string : parts) {
+			if (Integer.parseInt(string) >= 0 && Integer.parseInt(string) <= 1000) {
+				sum += Integer.parseInt(string);
+			} else if (Integer.parseInt(string) < 0) {
+				throw new StringCalculatorException();
+			}
+		}
+
+		return sum;
 	}
+
 }
--- src/it/uniba/tdd/tests/StringCalculatorTest.java
+++ src/it/uniba/tdd/tests/StringCalculatorTest.java
@@ -1,14 +1,84 @@
 package it.uniba.tdd.tests;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import it.uniba.tdd.StringCalculator;
+import it.uniba.tdd.StringCalculatorException;
 
 public class StringCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void emptyStringReturns0() throws StringCalculatorException {
+		int ris = StringCalculator.add("");
+
+		assertEquals(ris, 0);
+	}
+
+	@Test
+	public void oneNumberStringReturnsTheNumber() throws StringCalculatorException {
+		int ris = StringCalculator.add("3");
+
+		assertEquals(ris, 3);
+	}
+
+	@Test
+	public void twoNumbersStringReturnsTheirSum() throws StringCalculatorException {
+		int ris = StringCalculator.add("3,91");
+
+		assertEquals(ris, 94);
+	}
+
+	@Test
+	public void fiveNumbersStringReturnsTheirSum() throws StringCalculatorException {
+		int ris = StringCalculator.add("3,9,12,6");
+
+		assertEquals(ris, 30);
+	}
+	
+	@Test
+	public void numbersStringWithNewLineSeparatorWorks() throws StringCalculatorException{
+		int ris = StringCalculator.add("3,9,12\n6");
+
+		assertEquals(ris, 30);
+	}
+	
+	@Test
+	public void numbersStringWithNewDelimiterAtTheBeginningWorks() throws StringCalculatorException{
+		int ris = StringCalculator.add("//-\n1-2-7");
+		
+		assertEquals(ris, 10);
+	}
+	
+	@Test
+	public void numbersStringWithOneThousandDoesNotSum() throws StringCalculatorException{
+		int ris = StringCalculator.add("1,2,5,3,1001");
+		
+		assertEquals(ris,11);
+	}
+	
+	@Test(expected = StringCalculatorException.class)
+	public void negativeNumbersThrowsException() throws StringCalculatorException{
+		StringCalculator.add("1,2,5,-3");
+	}
+	
+	@Test(expected = StringCalculatorException.class)
+	public void numbersStringWithMinusAsDelimiterAndNegativeNumber() throws StringCalculatorException{
+		StringCalculator.add("//-\n1-2--7");
+	}
+	
+	@Test
+	public void numbersStringWithDelimiterWithTwoChars() throws StringCalculatorException{
+		int ris = StringCalculator.add("//[__]\n1__2__7");
+		
+		assertEquals(ris,10);
+	}
+	
+	@Test
+	public void numbersStringWithDelimitersWithTwoChars() throws StringCalculatorException{
+		int ris = StringCalculator.add("//[__][oP][,,]\n1__1002oP7,,90,,1");
+		
+		assertEquals(ris,99);
 	}
 
 }
